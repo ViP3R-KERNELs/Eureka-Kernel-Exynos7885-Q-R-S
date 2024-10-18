829c27d9f8976f05b22d57f7c669d3ae66ace126
IamCOD3X <sourabh.panchal28@gmail.com>
2024-10-18 21:41:12 +0530

    Adding missing definations for AirCrack-ng Drivers

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1728,7 +1728,6 @@ enum cfg80211_assoc_req_flags {
 	ASSOC_REQ_DISABLE_HT		= BIT(0),
 	ASSOC_REQ_DISABLE_VHT		= BIT(1),
 	ASSOC_REQ_USE_RRM		= BIT(2),
-	CONNECT_REQ_EXTERNAL_AUTH_SUPP	= BIT(3),
 };
 
 /**
@@ -2223,33 +2222,6 @@ struct cfg80211_qos_map {
 };
 
 /**
- * struct cfg80211_external_auth_params - Trigger External authentication.
- *
- * Commonly used across the external auth request and event interfaces.
- *
- * @action: action type / trigger for external authentication. Only significant
- *	for the authentication request event interface (driver to user space).
- * @bssid: BSSID of the peer with which the authentication has
- *	to happen. Used by both the authentication request event and
- *	authentication response command interface.
- * @ssid: SSID of the AP.  Used by both the authentication request event and
- *	authentication response command interface.
- * @key_mgmt_suite: AKM suite of the respective authentication. Used by the
- *	authentication request event interface.
- * @status: status code, %WLAN_STATUS_SUCCESS for successful authentication,
- *	use %WLAN_STATUS_UNSPECIFIED_FAILURE if user space cannot give you
- *	the real status code for failures. Used only for the authentication
- *	response command interface (user space to driver).
- */
-struct cfg80211_external_auth_params {
-	enum nl80211_external_auth_action action;
-	u8 bssid[ETH_ALEN] __aligned(2);
-	struct cfg80211_ssid ssid;
-	unsigned int key_mgmt_suite;
-	u16 status;
-};
-
-/*
  * struct cfg80211_ops - backend description for wireless configuration
  *
  * This struct is registered by fullmac card drivers and/or wireless stacks
@@ -2325,8 +2297,6 @@ struct cfg80211_external_auth_params {
  *	(invoked with the wireless_dev mutex held)
  * @leave_mesh: leave the current mesh network
  *	(invoked with the wireless_dev mutex held)
- * @external_auth: indicates result of offloaded authentication processing from
- *     user space
  *
  * @get_mesh_config: Get the current mesh configuration
  *
@@ -2786,8 +2756,6 @@ struct cfg80211_ops {
 	void	(*tdls_cancel_channel_switch)(struct wiphy *wiphy,
 					      struct net_device *dev,
 					      const u8 *addr);
-	int     (*external_auth)(struct wiphy *wiphy, struct net_device *dev,
-				 struct cfg80211_external_auth_params *params);
 };
 
 /*
@@ -5382,17 +5350,6 @@ wiphy_ext_feature_isset(struct wiphy *wiphy,
 
 /* ethtool helper */
 void cfg80211_get_drvinfo(struct net_device *dev, struct ethtool_drvinfo *info);
-
-/**
- * cfg80211_external_auth_request - userspace request for authentication
- * @netdev: network device
- * @params: External authentication parameters
- * @gfp: allocation flags
- * Returns: 0 on success, < 0 on error
- */
-int cfg80211_external_auth_request(struct net_device *netdev,
-				   struct cfg80211_external_auth_params *params,
-				   gfp_t gfp);
 
 /* Logging, debugging and troubleshooting/diagnostic helpers. */
 
